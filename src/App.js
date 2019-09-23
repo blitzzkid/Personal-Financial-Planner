432a32c6b2ef02bb14c17fe4e643fc7ef80f1855
mtan014 <mtan014@e.ntu.edu.sg>
2019-09-23 16:33:09 +0800

    Added React Routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import { Home } from "./Common/Home";
+import { Header } from "./Common/Header";
 import { RetirementPlanner } from "./Components/RetirementPlanner";
+import { HousingPlanner } from "./Components/HousingPlanner";
 
 export default function App() {
   return (
-    <div>
-      <RetirementPlanner />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Header />
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/retirement" component={RetirementPlanner} />
+          <Route exact path="/housing" component={HousingPlanner} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
---
+++ src/Components/HousingPlanner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HousingPlanner = () => {
+  return (<div>
+    Housing Plan
+  </div>)
+}
