97083003516063f5c518bb3c973a2cbf2a70e2b2
mtan014 <mtan014@e.ntu.edu.sg>
2019-09-25 00:35:44 +0800

    clean up fundsdata

--- src/Common/FundsData.js
+++ src/Common/FundsData.js
@@ -2,64 +2,62 @@ import React from "react";
 import { TableBodyRow } from "./FundsTable";
 import { localSampleData } from "../Assets/localsampledata";
 
-export class FundsData extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
+// export class FundsData extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       error: null,
+//       isLoaded: false,
+//       items: []
+//     };
+//   }
 
-  componentDidMount() {
-    fetch(
-      "https://api.worldtradingdata.com/api/v1/mutualfund?symbol=AAAAX,AAADX,AAAGX&api_token=5YKyMMqefD3AYohhMVnqZe7sJpzyAdcovhoDR0Ar9Jc691ZSZ3bRNNl7Tbgs"
-    )
-      .then(res => res.json())
-      .then(
-        resInJson => {
-          this.setState({
-            isLoaded: true,
-            items: resInJson.data
-          });
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-  }
+//   componentDidMount() {
+//     fetch(
+//       "https://api.worldtradingdata.com/api/v1/mutualfund?symbol=AAAAX,AAADX,AAAGX&api_token=5YKyMMqefD3AYohhMVnqZe7sJpzyAdcovhoDR0Ar9Jc691ZSZ3bRNNl7Tbgs"
+//     )
+//       .then(res => res.json())
+//       .then(
+//         resInJson => {
+//           this.setState({
+//             isLoaded: true,
+//             items: resInJson.data
+//           });
+//         },
+//         error => {
+//           this.setState({
+//             isLoaded: true,
+//             error
+//           });
+//         }
+//       );
+//   }
 
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <React.Fragment>Error: {error.message}</React.Fragment>;
-    } else if (!isLoaded) {
-      return <React.Fragment>Loading...</React.Fragment>;
-    } else {
-      return (
-        <React.Fragment>
-          <FundsList list={items} />
-        </React.Fragment>
-      );
-    }
-  }
-}
+//   render() {
+//     const { error, isLoaded, items } = this.state;
+//     if (error) {
+//       return <React.Fragment>Error:{error.message}</React.Fragment>;
+//     } else if (!isLoaded) {
+//       return <React.Fragment>Loading...</React.Fragment>;
+//     } else {
+//       return (
+//         <React.Fragment>
+//           <FundsList list={items} />
+//         </React.Fragment>
+//       );
+//     }
+//   }
+// }
 
-// export const FundsData = () => {
-//   return (
-//     <React.Fragment>
-//       <FundsList list={localSampleData} />
-//     </React.Fragment>
-//   );
-// };
+export const FundsData = () => {
+  return (
+    <React.Fragment>
+      <FundsList list={localSampleData.data} />
+    </React.Fragment>
+  );
+};
 
 export const FundsList = props => {
-  console.log(props.list);
-  console.log(typeof props.list);
   return (
     <React.Fragment>
       <TableBodyRow items={props.list} />
