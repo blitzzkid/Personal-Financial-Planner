6ad15ada0e0a5ebc69ddbf9019c49173be48abfd
mtan014 <mtan014@e.ntu.edu.sg>
2019-10-17 14:55:34 +0800

    Add Login functionality

--- src/Common/Users/Login.js
+++ src/Common/Users/Login.js
@@ -5,9 +5,19 @@ export class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      username: "",
+      password: ""
     };
   }
+
+  inputUsername = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  inputPassword = event => {
+    this.setState({ password: event.target.value });
+  };
 
   loginHandler = () => {
     const url = "http://localhost:3000/users/login";
@@ -15,13 +25,15 @@ export class Login extends React.Component {
       .post(
         url,
         {
-          username: "user126",
-          password: "password123"
+          username: this.state.username,
+          password: this.state.password
         },
         { withCredentials: true }
       )
       .then(res => {
         this.setState({ isLoggedIn: true });
+        this.setState({ username: "" });
+        this.setState({ password: "" });
       })
       .catch(err => {
         console.error(err);
@@ -42,11 +54,25 @@ export class Login extends React.Component {
   render() {
     return (
       <div>
-        <div className="loginInput">
-          <input type="text">Username</input>
-          <input type="text">Password</input>
+        <div className="login-form">
+          <label>
+            Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.inputUsername}
+            ></input>
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.inputPassword}
+            ></input>
+          </label>
+          <button onClick={this.loginHandler}>Login</button>
         </div>
-        <button onClick={this.loginHandler}>Login</button>
         <button onClick={this.logoutHandler}>Logout</button>
         <p>You are logged {this.state.isLoggedIn ? "in" : "out"}</p>
       </div>
